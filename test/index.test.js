182aebe5316799ca4bc8f066d66bd3f33a588206
Jahidul Islam <jsec516@gmail.com>
2017-01-22 15:50:42 +0600

    initialized i18n

---
+++ test/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect,
+    i18n = require('../index');
+
+describe('i18n', function() {
+  it('should return true', function() {
+    expect(i18n.t('errors.call.empty.url')).to.be.equal("no url provided from where we can get call content");
+  })
+})
